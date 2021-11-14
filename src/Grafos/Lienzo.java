7c2302e706b39eb8803e32c88e55cb84472d7aed
XimenaFlores <XimenaFlores@DESKTOP-LVPM7CI>
2021-11-13 20:21:09 -0600

    Se agregó clase Grafos, Lienzo y Nodo, y se modificó.

--- src/Grafos/Lienzo.java
+++ src/Grafos/Lienzo.java
@@ -1,5 +1,52 @@
 package Grafos;
 
-public class Lienzo {
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Vector;
+import javax.swing.JPanel;
+
+public class Lienzo extends JPanel implements MouseListener {  
+    private Vector<Nodo> vectorNodos;
     
+    public Lienzo(){
+        this.vectorNodos = new Vector<>();
+        this.addMouseListener(this);
+    }
+    
+    @Override
+    public void paint(Graphics g){
+        for(Nodo nodos : vectorNodos){
+            nodos.pintar(g);
+        }
+    }
+   
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(e.getButton() == MouseEvent.BUTTON1){
+            this.vectorNodos.add(new Nodo(e.getX(), e.getY()));
+            repaint();
+        }     
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }  
 }
