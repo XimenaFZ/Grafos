c703a874ad6cc19a1089f8eb92f6da9f5b6caeff
XimenaFlores <XimenaFlores@DESKTOP-LVPM7CI>
2021-11-13 20:35:09 -0600

    Se modificaron las clases.

--- src/Grafos/Lienzo.java
+++ src/Grafos/Lienzo.java
@@ -2,16 +2,20 @@ package Grafos;
 
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Vector;
 import javax.swing.JPanel;
 
 public class Lienzo extends JPanel implements MouseListener {  
-    private Vector<Nodo> vectorNodos;
+    private Vector<Nodo> vectorNodos;    
+    private Vector<Enlace> vectorEnlaces;
+    private Point p1, p2;
     
     public Lienzo(){
-        this.vectorNodos = new Vector<>();
+        this.vectorNodos = new Vector<>();       
+        this.vectorEnlaces = new Vector<>();
         this.addMouseListener(this);
     }
     
@@ -20,6 +24,9 @@ public class Lienzo extends JPanel implements MouseListener {
         for(Nodo nodos : vectorNodos){
             nodos.pintar(g);
         }
+        for(Enlace enlace : vectorEnlaces){
+            enlace.pintar(g);
+        }
     }
    
     @Override
@@ -27,7 +34,22 @@ public class Lienzo extends JPanel implements MouseListener {
         if(e.getButton() == MouseEvent.BUTTON1){
             this.vectorNodos.add(new Nodo(e.getX(), e.getY()));
             repaint();
-        }     
+        }    
+        if(e.getButton() == MouseEvent.BUTTON3){
+            for(Nodo nodo : vectorNodos){
+                if(new Rectangle(nodo.getX() - Nodo.d/2, nodo.getY() - Nodo.d/2, Nodo.d, Nodo.d).contains(e.getPoint())){
+                    if(p1 == null)
+                        p1 = new Point(nodo.getX(), nodo.getY());
+                    else{
+                        p2 = new Point(nodo.getX(), nodo.getY());
+                        this.vectorEnlaces.add(new Enlace(p1.x, p1.y, p2.x, p2.y));
+                        repaint();
+                        p1 = null;
+                        p2 = null;
+                    }
+                }
+            }
+        }
     }
 
     @Override
--- src/Grafos/Nodo.java
+++ src/Grafos/Nodo.java
@@ -14,4 +14,20 @@ public class Nodo {
     public void pintar(Graphics g){
         g.drawOval(this.x - d/2, this.y - d/2, d, d);
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
